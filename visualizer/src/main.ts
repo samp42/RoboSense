e2078467434095246fa871df804034d3bb68dc46
Samuel Proulx <samuelproulx26@gmail.com>
2023-05-25 19:46:03 -0400

    tauri app using solid

--- visualizer/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import VueKonva from 'vue-konva';
-
-const app = createApp(App);
-app.use(VueKonva);
-app.mount('#app');
